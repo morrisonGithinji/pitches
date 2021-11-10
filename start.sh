337f2e88f05d4ba5f8bda3e557ee699442d3f3c1
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-11-10 11:40:55 +0300

    add secret key

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-export SECRET_KEY='just a key'
-export MAIL_USERNAME='gakuoebenezer@gmail.com'
-export MAIL_PASSWORD='@ebay6100'
+export SECRET_KEY='5431'
+export MAIL_USERNAME='morrison.githinji@student.moringaschool.com'
+export MAIL_PASSWORD='5431140'
 export DATABASE_URL="postgres://xzxzdyjmnlzgih:1a1b6959abd5a5199d255c7729f0d030bfafe72f6a711cfee0c2ce206f626bbb@ec2-54-152-40-168.compute-1.amazonaws.com:5432/dfncuualnl3no3"
 
 
