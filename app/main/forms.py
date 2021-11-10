c1d9a68d5f9f12d9d9553e6d4a75863069eb1e07
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-11-10 10:45:25 +0300

    edit forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired
 from ..models import Pitch, Comment
 
 class PitchForm(FlaskForm):
-    category = SelectField('Category', choices = [('Job','Job'),('Pick-up','Pick-up'),('Band','Band'),('Business','Business')], validators = [Required()])
+    category = SelectField('Category', choices = [('Job','Job'),('Pick-up','Pick-up'),('Band','Band'),('Business','Business')], validators = [InputRequired()])
     title = StringField('Pitch title',validators=[InputRequired()])
     pitch = TextAreaField('Pitch', )
     submit = SubmitField('Submit')
